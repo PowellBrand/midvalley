e894479880443be00d9d82ce99c5a52c9caeff2d
PowellBrand <powellbrandon@outlook.com>
2018-03-28 11:16:47 -0600

    front-end folder made

---
+++ src/components/district/district.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class District extends Component{
+
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default District
