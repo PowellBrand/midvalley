4919addfbfd07ec734ba9faf0451d9179f0180eb
PowellBrand <powellbrandon@outlook.com>
2018-04-05 11:52:45 +0200

    started contractors

--- src/components/contractors/contractors.js
+++ src/components/contractors/contractors.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Contractors extends Component{
+class Contractors extends Component {
 
 
-    render(){
-        return(
-            <div>
-                
+    render() {
+        return (
+            <div className="mainBody">
+                <div className="p-div">
+                    <h1>CONTRACTORS</h1>
+                    <p>Midvalley Improvement Distric has approved Design Standards and Construction Specifications, detailed drawings, general design and construction processes, and required impact fees. The following are available to download:</p>
+                </div>
             </div>
         )
     }
